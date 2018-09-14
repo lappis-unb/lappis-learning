dfdf2ceed9075cb0d4d99dd266a71d7075892bd1
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2018-09-14 15:33:24 -0300

    Makes number_of_items use DataSource

--- salicml/features/number_of_items.py
+++ salicml/features/number_of_items.py
@@ -7,22 +7,9 @@ class FeatureNumberOfItems:
 
 
     def __init__(self):
-        pass
+        self.data_source = DataSource()
+
 
     def get_number_of_items(self, pronac = ""):
-        query = (
-            "SELECT DISTINCT CONCAT(p.AnoProjeto, p.Sequencial) as PRONAC, \
-             p.Segmento as idSegmento, \
-             COUNT(a.Item) OVER (PARTITION BY a.idPronac) as number_of_items \
-             FROM SAC.dbo.Projetos as p \
-             LEFT JOIN SAC.dbo.vwPlanilhaAprovada a \
-             ON p.IdPRONAC = a.idPronac"
-        )
-
-        if pronac:
-            query += " WHERE a.PRONAC = '{0}'".format(pronac)
-            return self.execute_query(query)[0]
-
-        else:
-            return self.execute_query(query)
+        return self.data_source.get_pronac_segmento_number_of_items(pronac)
 
