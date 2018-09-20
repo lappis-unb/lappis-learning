9ce118fbaaf91c35b12a9f5bca30362c0e3ffad1
Marlon Mendes <marlonciriatico@gmail.com>
2018-09-20 17:37:39 -0300

    Tests for feature extraction for multiple projects

--- salicml/tests/features/test_number_of_items.py
+++ salicml/tests/features/test_number_of_items.py
@@ -23,7 +23,15 @@ class TestFeatureNumberOfItems(unittest.TestCase):
     def test_get_one_pronac_number_of_items(self):
         feature = FeatureNumberOfItems()
         number_of_items = feature.get_pronac_number_of_items(self.pronac_items)
-        print('nm = {}'.format(number_of_items))
         self.assertEqual(len(number_of_items), 3)
         self.assertEqual(number_of_items[2], 3)
 
+    def test_get_pronacs_number_of_items(self):
+        feature = FeatureNumberOfItems()
+        pronacs_features = \
+            feature.get_projects_number_of_items(self.projects_items)
+
+        self.assertEqual(len(pronacs_features), 2)
+
+        expected_result = [['012345', '2A', 3], ['012348', '3A', 2], ]
+        self.assertEquals(pronacs_features, expected_result)
