5da58349b368f0239518ce40f146b2655eba0a5e
Marlon Mendes <marlonciriatico@gmail.com>
2018-09-20 17:33:28 -0300

    Separating datasource from feature extraction

--- salicml/tests/features/test_number_of_items.py
+++ salicml/tests/features/test_number_of_items.py
@@ -5,13 +5,25 @@ from salicml.features.number_of_items import FeatureNumberOfItems
 
 class TestFeatureNumberOfItems(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.projects_items = [['012345', '2A', 123],
+                              ['012345', '2A', 124],
+                              ['012345', '2A', 125],
+                              ['012348', '3A', 126],
+                              ['012348', '3A', 127], ]
+
+        cls.pronac_items = [['012345', '2A', 123],
+                            ['012345', '2A', 124],
+                            ['012345', '2A', 125], ]
+
 
     def test_get_one_pronac_number_of_items(self):
-        pronac = '164274'
         feature = FeatureNumberOfItems()
-        number_of_items = feature.get_number_of_items(pronac)
-
-
-        self.assertEqual(3, len(number_of_items))
-        self.assertEqual(pronac, number_of_items[0])
+        number_of_items = feature.get_pronac_number_of_items(self.pronac_items)
+        print('nm = {}'.format(number_of_items))
+        self.assertEqual(len(number_of_items), 3)
+        self.assertEqual(number_of_items[2], 3)
 
