cd5e8cb10420372f4365f0971243596591a4b3d6
Marlon Mendes <marlonciriatico@gmail.com>
2018-09-25 14:20:36 -0300

    PEP8 refactoring, adding docstring

--- salicml/middleware/number_of_items.py
+++ salicml/middleware/number_of_items.py
@@ -7,11 +7,10 @@ from salicml.middleware.exceptions import TraningNotFound
 from math import ceil
 
 
-def str_int(n):
-    return str(int(n))
-
-
 class NumberOfItemsMiddleware:
+    '''This class is a middleware specialist in the metric NumberOfItems.
+    It gets all necessary raw data from DataSource, extracts the feature
+    NumberOfItems, and makes inference on that feature'''
 
     FILE_PATH = os.path.dirname(os.path.realpath(__file__))
     TRAIN_FOLDER = os.path.join(FILE_PATH, 'trainings')
@@ -24,6 +23,8 @@ class NumberOfItemsMiddleware:
         self.number_of_items = NumberOfItemsModel()
 
     def fill_json(self, metric_result):
+        '''Add keys on the metric inference result json so the new json is
+        ready to be consumed by a front-end'''
 
         METRIC_GOOD = 'Metric-good'
         METRIC_BAD = 'Metric-bad'
@@ -52,14 +53,20 @@ class NumberOfItemsMiddleware:
         return json
 
     def load_number_of_items(self):
+        '''Tries to load the training model for the feature'''
         self.number_of_items.load(
             NumberOfItemsMiddleware.TRAIN_NUMBER_OF_METRICS_PATH,
             self.on_load_number_of_items_error)
 
     def on_load_number_of_items_error(self):
+        '''Callback function on the case of there is no trained stored to be
+        loaded'''
         raise TraningNotFound('Number of Items training not found')
 
     def train_number_of_items(self, planilha_orcamentaria, save=True):
+        '''Extracts the feature, trains a model on the extracted feature and
+        returns the trained model. If save=True, the trained model will also
+         be saved as a .picke file'''
         feature = FeatureNumberOfItems()
 
         items_features = feature.get_projects_number_of_items(
@@ -72,6 +79,9 @@ class NumberOfItemsMiddleware:
                 NumberOfItemsMiddleware.TRAIN_NUMBER_OF_METRICS_PATH)
 
     def get_metric_number_of_items(self, pronac):
+        '''Makes inference and calculate the metric number of items for the
+        given pronac. The pronac's data will downloaded from the SALIC database
+        so its guaranted to be up-to-date.'''
         feature = FeatureNumberOfItems()
         where = 'WHERE a.PRONAC = \'{0}\''.format(pronac)
         planilha_orcamentaria = self._data_source.get_planilha_orcamentaria(
@@ -83,5 +93,8 @@ class NumberOfItemsMiddleware:
 
         result = self.number_of_items.is_outlier(number_of_items, id_segment)
         result['number_of_items'] = number_of_items
-        result_json = self.fill_json(result)
-        return result_json
+        return result
+
+
+def str_int(n):
+    return str(int(n))
